5264fa765b6c76729701177659575e23a9b1fc9f
Kiet CHau <kchau@vivid-hosting.net>
2018-10-07 10:04:00 -0700

    Initial working for drac6

---
+++ drac6.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo -n 'Host: '
+read drachost
+
+dracuser="vividhosting"
+
+echo -n 'Password: '
+read -s dracpwd
+echo
+
+./jre/bin/java -cp avctKVM.jar -Djava.library.path=./lib com.avocent.idrac.kvm.Main ip=$drachost kmport=5900 vport=5900 user=$dracuser passwd=$dracpwd apcp=1 version=2 vmprivilege=true "helpurl=https://$drachost:443/help/contents.html"
